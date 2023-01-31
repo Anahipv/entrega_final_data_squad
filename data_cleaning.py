91849e9c887b369426f752b306a8f9a54fbf6ce6
Anahi Perez Valenzuela <perezvalenzuela.anahi@gmail.com>
2023-01-31 01:51:10 +0100

    Features converted to columns with Booleans assigned

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,11 +3,11 @@ import pandas as pd
 df = pd.read_csv("airbnb-listings.csv", sep=";")
 rows, columns = df.shape
 
-print(f"We have {rows} rows and {columns} columns")
+# print(f"We have {rows} rows and {columns} columns")
 
 column_names = list(df)
 
-print(column_names)
+# print(column_names)
 
 columns_kept = ["ID", "Host ID", "Host Name", "Host Response Rate", "Street", 
 "Neighbourhood Cleansed", "Neighbourhood Group Cleansed", "City", "State", "Zipcode", "Smart Location",
@@ -22,9 +22,12 @@ df_with_city = df_less_columns[df_less_columns.City.notnull()]
 #remove airbnbs not in Madrid
 df_madrid = df_with_city[df_with_city["City"].str.contains("Mad")]
 
+# print(df_madrid.head())
+# print(df_madrid.iloc[[1]])
+
 ##remove shared rooms
 
-important_features = ["Host Is SuperHost", "Host Identity Verified"]
+important_features = ["Host Is Superhost", "Host Identity Verified"]
 
 ##internet and wireless should be combined in one column, maybe washer and dryer as well
 important_amenities = ["Kitchen", "Internet", "Wireless", "Air conditioning", "Heating", "Washer", "Dryer", "Elevator"]
@@ -36,5 +39,15 @@ cancelation_policy = {"strict": 0, "moderate": 1, "flexible": 2}
 
 
 ##adding columns for important features
+
+df_madrid_2 = df_madrid
+df_madrid_2 = df_madrid_2.reset_index()
+
+## CONVERTIR A FUNCION ASI SE USA PARA AMENITIES Y FEATURES
 for feature in important_features:
-    df_madrid[feature] = df_madrid.loc[df_madrid["Features"].str.contains(feature)]
+    df_madrid_2[feature] = False
+    booleans = list(df_madrid_2["Features"].str.contains(feature))
+    for i in range(len(df_madrid)):
+        df_madrid_2[feature].iat[i] = booleans[i]
+
+print(df_madrid_2.head())
