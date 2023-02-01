9ead709913003df8cc2c67672e0c63a158cdc5c6
CGraciolli <cgraciolli@gmail.com>
2023-02-01 12:08:30 +0100

    barplot type room

--- graphs.py
+++ graphs.py
@@ -15,20 +15,20 @@ for amenity in important_amenities:
 rows = df_madrid.shape[0]
 list_percentage = list(map(lambda x: int(100*x/rows), list_count))
 
-bp = sns.barplot(x=important_amenities, y=list_percentage)
-bp.set_title("Percentage of properties with amenities", fontsize=30)
-for i in range(len(important_amenities)):
-    plt.text(i,list_percentage[i]/2, str(list_percentage[i])+"%", color="white", fontweight="bold", horizontalalignment="center")
+#bp = sns.barplot(x=important_amenities, y=list_percentage)
+#bp.set_title("Percentage of properties with amenities", fontsize=30)
+#for i in range(len(important_amenities)):
+    #plt.text(i,list_percentage[i]/2, str(list_percentage[i])+"%", color="white", fontweight="bold", horizontalalignment="center")
 #plt.xticks(rotation=90, fontsize=10)
 #plt.show()
 
-dict_freq = {}
-for index in range(len(df_madrid)):
-    type_prop = df_madrid["Room Type"].iat[index]
-    if type_prop in dict_freq:
-        dict_freq[type_prop] += 1
-    else:
-        dict_freq[type_prop] = 1
+#dict_freq = {}
+#for index in range(len(df_madrid)):
+    #type_prop = df_madrid["Room Type"].iat[index]
+    #if type_prop in dict_freq:
+        #dict_freq[type_prop] += 1
+    #else:
+        #dict_freq[type_prop] = 1
 
 #sns.scatterplot(x="Neighbourhood Group Cleansed", y="Price", data=df_madrid, hue="Room Type")
 #plt.xticks(rotation=90, fontsize=10)
@@ -40,12 +40,21 @@ for index in range(len(df_madrid)):
 #bx.set_yticks(range(0, 250, 10))
 #plt.show()
 
-bx = sns.boxplot(x=["Air conditioning", "Pool", "Breakfast"], y="Price", data=df_madrid, fliersize=0.5)
-bx.grid(axis="y", color="gray", linestyle="dashed")
-plt.ylim(ymax=250, ymin=0)
-bx.set_yticks(range(0, 250, 10))
+#bx = sns.boxplot(x=["Air conditioning", "Pool", "Breakfast"], y="Price", data=df_madrid, fliersize=0.5)
+#bx.grid(axis="y", color="gray", linestyle="dashed")
+#plt.ylim(ymax=250, ymin=0)
+#bx.set_yticks(range(0, 250, 10))
 #plt.show()
 
-neighborhood_groups = df_madrid["Neighbourhood Group Cleansed"]
-print(neighborhood_groups)
+room_types = df_madrid["Room Type"].unique()
+list_df_room_types = []
+for room_type in room_types:
+    df_filtered = df_madrid[df_madrid["Room Type"] == room_type]
+    list_df_room_types.append(df_filtered)
+
+for index, df in enumerate(list_df_room_types):
+    rt = sns.barplot(x="Neighbourhood Group Cleansed", y="Price", data=df)
+    rt.set_title(room_types[index], fontsize=30)
+    plt.xticks(rotation=90, fontsize=10)
+    plt.show()
 
