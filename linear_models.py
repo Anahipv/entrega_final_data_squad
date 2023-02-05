3fc76b4de395afca7a576ca883a9946559e3b73f
CGraciolli <cgraciolli@gmail.com>
2023-02-05 11:11:51 +0100

    starting to deal with nan values

--- linear_models.py
+++ linear_models.py
@@ -9,8 +9,17 @@ df_madrid = pd.read_csv("airbnb_madrid_clean.csv")
 
 ##first we remove the columns that won't be used in the linear model
 df_madrid = remove_columns(["Host ID", "Host Name", "Street", "Neighbourhood Cleansed", "City", "State", "Bed Type",
-"Zipcode", "Country", "Latitude", "Longitude", "Amenities Rating", "Host Is Superhost", "Host Identity Verified"], df_madrid)
+"Zipcode", "Country", "Latitude", "Longitude", "Amenities Rating", "Host Is Superhost", "Host Identity Verified",
+"Review Scores Rating", "Host Response Rate"], df_madrid)
 df_madrid = remove_columns(important_amenities, df_madrid)
+
+
+##remove row with nan values in beds, bathroms and bedrooms
+df_madrid = df_madrid.dropna(subset=["Beds", "Bathrooms", "Bedrooms"])
+
+##we need to deal with nan values in security deposit and cleaning fee
+##suggestion: median considering the number of rooms
+
 
 ##then we make dummie variables for the categorical columns
 cat_columns = ["Neighbourhood Group Cleansed", "Property Type", "Room Type", "Cancellation Policy"]
@@ -25,16 +34,23 @@ target = df_madrid["Price"]
 predictors = df_madrid.drop(columns = ["Price"])
 X_train, X_test, y_train, y_test = train_test_split(predictors, target, test_size=0.3, random_state=40)
 
+
+total = X_train.isnull().sum().sort_values(ascending=False)
+percent = (X_train.isnull().sum()/X_train.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+print(missing_data.head(20))
+
+
 ##this should work after removing Nans
-lr = LinearRegression()
-lr.fit(X_train, y_train)
+#lr = LinearRegression()
+#lr.fit(X_train, y_train)
 
 ##making predictions
-y_pred = lr.predict(X_test)
+#y_pred = lr.predict(X_test)
 
-_preds_df = pd.DataFrame(dict(observed=y_test, predicted=y_pred))
-_preds_df.head()
+#_preds_df = pd.DataFrame(dict(observed=y_test, predicted=y_pred))
+#_preds_df.head()
 
 ##evaluating the model
-print('Score: {}'.format(lr.score(X_test, y_test)))
-print('MSE: {}'.format(mean_squared_error(y_test, y_pred)))
+#print('Score: {}'.format(lr.score(X_test, y_test)))
+#print('MSE: {}'.format(mean_squared_error(y_test, y_pred)))
