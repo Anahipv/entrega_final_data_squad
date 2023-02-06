1a7c578abcca8d8907851fab3f7a74e7d05d8af4
Anahi Perez Valenzuela <perezvalenzuela.anahi@gmail.com>
2023-02-07 00:18:44 +0100

    algunas pruebas en pruebas.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,20 +1,20 @@
 import pandas as pd
 from functions import create_and_populate_columns, remove_columns
 
-df = pd.read_csv("airbnb-listings.csv", sep=";")
+df = pd.read_csv('airbnb-listings.csv', sep=';')
 rows, columns = df.shape
 
-print(f"We have {rows} rows and {columns} columns")
+print(f'We have {rows} rows and {columns} columns')
 
 column_names = list(df)
 
 print(column_names)
 
-columns_kept = ["ID", "Host ID", "Host Name", "Host Response Rate", "Street", 
-"Neighbourhood Cleansed", "Neighbourhood Group Cleansed", "City", "State", "Zipcode",
-"Country", "Latitude", "Longitude", "Property Type", "Room Type", "Accommodates", "Bathrooms", "Bedrooms", 
-"Beds", "Bed Type", "Amenities", "Price", "Security Deposit", "Cleaning Fee", "Guests Included",
-"Number of Reviews", "Review Scores Rating", "Cancellation Policy", "Features"]
+columns_kept = ['ID', 'Host ID', 'Host Name', 'Host Response Rate', 'Street', 
+'Neighbourhood Cleansed', 'Neighbourhood Group Cleansed', 'City', 'State', 'Zipcode',
+'Country', 'Latitude', 'Longitude', 'Property Type', 'Room Type', 'Accommodates', 'Bathrooms', 'Bedrooms', 
+'Beds', 'Bed Type', 'Amenities', 'Price', 'Security Deposit', 'Cleaning Fee', 'Guests Included',
+'Number of Reviews', 'Review Scores Rating', 'Cancellation Policy', 'Features']
 
 
 df_less_columns = df.filter(columns_kept, axis=1)
@@ -22,58 +22,55 @@ df_less_columns = df.filter(columns_kept, axis=1)
 df_with_city = df_less_columns[df_less_columns.City.notnull()]
 
 #remove airbnbs not in Madrid
-df_madrid = df_with_city[df_with_city["City"].str.contains("Mad", case=False)]
+df_madrid = df_with_city[df_with_city['City'].str.contains('Mad', case=False)]
 
 
 amenities = set()
-list_amenities = list(df_madrid["Amenities"])
+list_amenities = list(df_madrid['Amenities'])
 for item in list_amenities:
     if type(item) == str:
-        new_list = item.split(",")
+        new_list = item.split(',')
         for amenity in new_list:
             amenities.add(amenity)
 
 
-important_features = ["Host Is Superhost", "Host Identity Verified"]
-important_amenities = ["Kitchen", "Internet", "Wireless", "Air conditioning", "Heating", "Washer", 
-"Dryer", "Elevator", 'Wheelchair accessible', "TV", "Pool", '24-hour check-in']
+important_features = ['Host Is Superhost', 'Host Identity Verified']
+important_amenities = ['Kitchen', 'Internet', 'Wireless', 'Air conditioning', 'Heating', 'Washer', 
+'Dryer', 'Elevator', 'Wheelchair accessible', 'TV', 'Pool', '24-hour check-in']
 
 ##we group the data in property types like apartment, house or other
-relevant_types = ["Apartment", "House"]
+relevant_types = ['Apartment', 'House']
 for index in range(len(df_madrid)):
-    if df_madrid["Property Type"].iat[index] not in relevant_types:
-        df_madrid["Property Type"].iat[index] = "Other"
+    if df_madrid['Property Type'].iat[index] not in relevant_types:
+        df_madrid['Property Type'].iat[index] = 'Other'
 
 
 ##resetting indexes
 df_madrid.reset_index(drop=True, inplace=True)
 
 ##adding columns for important features and amenities
-df_madrid = create_and_populate_columns(important_features, "Features", df_madrid)
+df_madrid = create_and_populate_columns(important_features, 'Features', df_madrid)
 
-df_madrid = create_and_populate_columns(important_amenities, "Amenities", df_madrid)
+df_madrid = create_and_populate_columns(important_amenities, 'Amenities', df_madrid)
 
-##join columns "Internet" and "Wireless" under the internet column
+
+##join columns 'Internet' and 'Wireless' under the internet column
 for i in range(len(df_madrid)):
-    df_madrid["Internet"].iat[i] = df_madrid["Internet"].iat[i] and df_madrid["Wireless"].iat[i]
+    df_madrid['Internet'].iat[i] = df_madrid['Internet'].iat[i] and df_madrid['Wireless'].iat[i]
 
 
 ##removing the original columns for Features, Amenities and Wireless
-df_madrid = remove_columns(["Features", "Amenities", "Wireless"], df_madrid)
+df_madrid = remove_columns(['Features', 'Amenities', 'Wireless'], df_madrid)
 
-important_amenities.remove("Wireless")
+important_amenities.remove('Wireless')
 
-#cleaning column "Zipcode" 
-
-print(df_madrid[['ID', "Neighbourhood Cleansed", "Neighbourhood Group Cleansed", "City", "State", "Zipcode"]]
-        [(df_madrid['Zipcode'].isnull()) | (df_madrid['Zipcode'] == '-')])
-
+#cleaning column 'Zipcode' 
 df_madrid.loc[(df_madrid['Zipcode'].isnull()) | (df_madrid['Zipcode'] == '-'), 'Zipcode'] = ''
 
 corr = df_madrid.corr()
 dict_weights = {}
 for amenity in important_amenities:
-    weight = corr["Price"][amenity]
+    weight = corr['Price'][amenity]
     dict_weights[amenity] = weight
 
 scores = []
@@ -84,23 +81,23 @@ for index in range(len(df_madrid)):
             score += dict_weights[amenity]
     scores.append(score)
 
-df_madrid["Amenities Score"] = scores
+df_madrid['Amenities Score'] = scores
 
-print(df_madrid["Amenities Score"])
+print(df_madrid['Amenities Score'])
 
-q3 = df_madrid["Amenities Score"].quantile(0.33)
-q6 = df_madrid["Amenities Score"].quantile(0.67)
+q3 = df_madrid['Amenities Score'].quantile(0.33)
+q6 = df_madrid['Amenities Score'].quantile(0.67)
 
 ratings = []
 for index in range(len(df_madrid)):
-    if df_madrid["Amenities Score"].iat[index] < q3:
-        ratings.append("C")
-    elif df_madrid["Amenities Score"].iat[index] < q6:
-        ratings.append("B")
+    if df_madrid['Amenities Score'].iat[index] < q3:
+        ratings.append('C')
+    elif df_madrid['Amenities Score'].iat[index] < q6:
+        ratings.append('B')
     else:
-        ratings.append("A")
-df_madrid["Amenities Rating"] = ratings
+        ratings.append('A')
+df_madrid['Amenities Rating'] = ratings
 
 
 ##convert the dataset to csv
-df_madrid.to_csv("airbnb_madrid_clean.csv")
+df_madrid.to_csv('airbnb_madrid_clean.csv')
--- pruebas.py
+++ pruebas.py
@@ -6,19 +6,24 @@ def create_and_populate_columns(list_column, column_reference, df):
             df[item].iat[i] = booleans[i]
     return df
 
-def create_columns(df, list_columns):
-    for item in list_columns:
-        df[item] = None
+def create_column(df, column):
+    df[column] = None
     return df
 
 def column_contains(df, column_reference, item):
     booleans = list(df[column_reference].str.contains(item, case=False))
     return booleans
 
-def populate_columns(df, column, list_booleans):
+def populate_column_with_list(df, column, list_data):
     for i in range(len(df)):
-        df[column].iat[i] = list_booleans[i]
+        df[column].iat[i] = list_data[i]
     return df
 
-# def create_and_populate_columns2(list_columns, column_reference, df):
-#     df = create_columns(df, list_columns)
+def create_and_populate_columns2(list_column, column_reference, df):
+    for column in list_column:
+        df = create_column(df, column)
+        booleans = column_contains(df, column_reference, column)
+        df = populate_column_with_list(df, column, booleans)
+    return df
+
+    
