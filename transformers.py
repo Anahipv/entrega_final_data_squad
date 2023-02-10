00e2b4f7e9a394d47d65471172254a48542470b4
CGraciolli <cgraciolli@gmail.com>
2023-02-10 09:44:04 +0100

    removed dropnan from transformers

--- transformers.py
+++ transformers.py
@@ -3,26 +3,6 @@ import pandas as pd
 from functions import remove_columns, change_nan_to_median, create_median_dict
 from sklearn.preprocessing import OneHotEncoder
 
-class DropNans(TransformerMixin):
-    def __init__(self):
-        super().__init__()
-
-    def fit(self, X, y):
-        self.X = X
-        self.y = y
-        return self
-
-    def transform(self, X, y):
-        # drop incomplete rows
-        columns = ['Beds', 'Bathrooms', 'Bedrooms', 'Price', 'Review Scores Rating', 'Host Response Rate']
-        Xp = pd.DataFrame(X)
-        Yp = pd.DataFrame(y)
-        data = Xp.join(Yp)
-        datadrop = data.dropna(subset=columns).to_numpy()
-        Ydrop = datadrop['Price']
-        Xdrop = datadrop.drop('Price', axis = 1)
-        return Xdrop, Ydrop
-
 
 class ImputeMedian(BaseEstimator, TransformerMixin):
     def __init__(self):
